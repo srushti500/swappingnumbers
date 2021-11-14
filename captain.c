54d2b392f8376b8514f66e08087111c27faeb122
srushti500 <94307773+srushti500@users.noreply.github.com>
2021-11-14 22:07:20 +0530

    Create captain.c
    This file prints swapping of two numbers on the screen.

---
+++ captain.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int x,y;
+  printf("Enter the value of x and y\n\n");
+  scanf("%d%d",&x&y);
+  printf("The value of x and y are %d and %d respectively BEFORE swapping/n",x,y);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  printf("The value of x and y are %d and %d respectively AFTER swapping/n",x,y);
+  return 0;
+}  
